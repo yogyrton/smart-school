0bcd74b87be9d8d996f2826f060f9e1824ae4487
EgorParusov <parusov.93@mail.ru>
2022-08-02 15:04:02 +0300

    change price in admin panel

--- app/Traits/PhotoTrait.php
+++ app/Traits/PhotoTrait.php
@@ -10,9 +10,4 @@ trait PhotoTrait
     {
         return Photo::query()->where('camp', '=', $camp)->get();
     }
-
-    public static function getAllPhoto()
-    {
-        return Photo::all();
-    }
 }
